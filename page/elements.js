f584966067e878e7cb9a07afe9781527e1fe96e6
Robin <rehill012@gmail.com>
2025-03-04 09:54:14 -0500

    Add tab functionality

--- page/elements.js
+++ page/elements.js
@@ -1,5 +1,36 @@
 import { manifest } from "./systems/list.js"
 
+// Tabs ==============================================
+
+function selectTab(event, tabName, showConverter) {
+	// Hide tab contents
+	const contents = document.getElementsByClassName("tab_content")
+	for (var index = 0; index < contents.length; index++) {
+		contents[index].style.display = "none"
+	}
+
+	// Deactivate all tab buttons
+	const buttons = document.getElementsByClassName("tab_button")
+	for (var index = 0; index < buttons.length; index++) {
+		buttons[index].className = buttons[index].className.replace(" active", "")
+	}
+
+	// Show current tab contents and activate tab
+	document.getElementById(tabName).style.display = "block"
+	event.currentTarget.className += " active"
+
+	// Toggle conversion table
+	const table = document.getElementById("conversionTable")
+	if (showConverter) {
+		table.style.display = "block"
+	} else {
+		table.style.display = "none"
+	}
+}
+
+// System preset selector ============================
+
+// Populates the system selector from the system list
 function populate_select(select_id) {
 	const sel = document.getElementById(select_id)
 	for (const index in manifest) {
@@ -11,9 +42,10 @@ function populate_select(select_id) {
 	}
 }
 
+// Returns the currently selected element of the given select element
 function get_selected(select_id) {
 	const sel = document.getElementById(select_id)
 	return sel.value
 }
 
-export { populate_select, get_selected }
+export { selectTab, populate_select, get_selected }
