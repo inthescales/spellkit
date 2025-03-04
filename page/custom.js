5987d8fd191081bfa27cc156f3a5f1248eb69d58
Robin <rehill012@gmail.com>
2025-03-04 18:13:36 -0500

    Streamline custom.js

--- page/custom.js
+++ page/custom.js
@@ -1,142 +1,75 @@
 import System from "./systems/system.js"
 
+const input_pairs = [
+	["ɑ", document.getElementById("aa")],
+	["æ", document.getElementById("ae")],
+	["ʌ", document.getElementById("uh")],
+	["ɔ", document.getElementById("aw")],
+	["aʊ", document.getElementById("au")],
+	["aɪ", document.getElementById("ai")],
+	["ɛ", document.getElementById("e")],
+	["ɝ", document.getElementById("er")],
+	["ə", document.getElementById("schwa")],
+	["ɚ", document.getElementById("schwar")],
+	["eɪ", document.getElementById("ei")],
+	["ɪ", document.getElementById("ih")],
+	["i", document.getElementById("ee")],
+	["oʊ", document.getElementById("oa")],
+	["ɔɪ", document.getElementById("oi")],
+	["ʊ", document.getElementById("horseshoe")],
+	["u", document.getElementById("oo")],
+	["b", document.getElementById("b")],
+	["tʃ", document.getElementById("ch")],
+	["d", document.getElementById("d")],
+	["ð", document.getElementById("dh")],
+	["f", document.getElementById("f")],
+	["g", document.getElementById("g")],
+	["h", document.getElementById("h")],
+	["dʒ", document.getElementById("j")],
+	["k", document.getElementById("k")],
+	["l", document.getElementById("l")],
+	["m", document.getElementById("m")],
+	["n", document.getElementById("n")],
+	["ŋ", document.getElementById("ng")],
+	["p", document.getElementById("p")],
+	["ɹ", document.getElementById("r")],
+	["s", document.getElementById("s")],
+	["ʃ", document.getElementById("sh")],
+	["t", document.getElementById("t")],
+	["θ", document.getElementById("th")],
+	["v", document.getElementById("v")],
+	["w", document.getElementById("w")],
+	["j", document.getElementById("y")],
+	["z", document.getElementById("z")],
+	["ʒ", document.getElementById("zh")],
+]
+
 function get_graphs() {
-	const graphs = {
-		"ɑ": document.getElementById("aa").value,
-		"æ": document.getElementById("ae").value,
-		"ʌ": document.getElementById("uh").value,
-		"ɔ": document.getElementById("aw").value,
-		"aʊ": document.getElementById("au").value,
-		"aɪ": document.getElementById("ai").value,
-		"ɛ": document.getElementById("e").value,
-		"ɝ": document.getElementById("er").value,
-		"ə": document.getElementById("schwa").value,
-		"ɚ": document.getElementById("schwar").value,
-		"eɪ": document.getElementById("ei").value,
-		"ɪ": document.getElementById("ih").value,
-		"i": document.getElementById("ee").value,
-		"oʊ": document.getElementById("oa").value,
-		"ɔɪ": document.getElementById("oi").value,
-		"ʊ": document.getElementById("horseshoe").value,
-		"u": document.getElementById("oo").value,
-		"b": document.getElementById("b").value,
-		"tʃ": document.getElementById("ch").value,
-		"d": document.getElementById("d").value,
-		"ð": document.getElementById("dh").value,
-		"f": document.getElementById("f").value,
-		"g": document.getElementById("g").value,
-		"h": document.getElementById("h").value,
-		"dʒ": document.getElementById("j").value,
-		"k": document.getElementById("k").value,
-		"l": document.getElementById("l").value,
-		"m": document.getElementById("m").value,
-		"n": document.getElementById("n").value,
-		"ŋ": document.getElementById("ng").value,
-		"p": document.getElementById("p").value,
-		"ɹ": document.getElementById("r").value,
-		"s": document.getElementById("s").value,
-		"ʃ": document.getElementById("sh").value,
-		"t": document.getElementById("t").value,
-		"θ": document.getElementById("th").value,
-		"v": document.getElementById("v").value,
-		"w": document.getElementById("w").value,
-		"j": document.getElementById("y").value,
-		"z": document.getElementById("z").value,
-		"ʒ": document.getElementById("zh").value,
+	let graphs = {}
+
+	for (const index in input_pairs) {
+		const pair = input_pairs[index]
+		graphs[pair[0]] = pair[1].value
 	}
 
 	return graphs
 }
 
 function set_graphs(system) {
-	document.getElementById("aa").value = system.get_graph("ɑ")
-	document.getElementById("ae").value = system.get_graph("æ")
-	document.getElementById("uh").value = system.get_graph("ʌ")
-	document.getElementById("aw").value = system.get_graph("ɔ")
-	document.getElementById("au").value = system.get_graph("aʊ")
-	document.getElementById("ai").value = system.get_graph("aɪ")
-	document.getElementById("e").value = system.get_graph("ɛ")
-	document.getElementById("er").value = system.get_graph("ɝ")
-	document.getElementById("schwa").value = system.get_graph("ə")
-	document.getElementById("schwar").value = system.get_graph("ɚ")
-	document.getElementById("ei").value = system.get_graph("eɪ")
-	document.getElementById("ih").value = system.get_graph("ɪ")
-	document.getElementById("ee").value = system.get_graph("i")
-	document.getElementById("oa").value = system.get_graph("oʊ")
-	document.getElementById("oi").value = system.get_graph("ɔɪ")
-	document.getElementById("horseshoe").value = system.get_graph("ʊ")
-	document.getElementById("oo").value = system.get_graph("u")
-	document.getElementById("b").value = system.get_graph("b")
-	document.getElementById("ch").value = system.get_graph("tʃ")
-	document.getElementById("d").value = system.get_graph("d")
-	document.getElementById("dh").value = system.get_graph("ð")
-	document.getElementById("f").value = system.get_graph("f")
-	document.getElementById("g").value = system.get_graph("g")
-	document.getElementById("h").value = system.get_graph("h")
-	document.getElementById("j").value = system.get_graph("dʒ")
-	document.getElementById("k").value = system.get_graph("k")
-	document.getElementById("l").value = system.get_graph("l")
-	document.getElementById("m").value = system.get_graph("m")
-	document.getElementById("n").value = system.get_graph("n")
-	document.getElementById("ng").value = system.get_graph("ŋ")
-	document.getElementById("p").value = system.get_graph("p")
-	document.getElementById("r").value = system.get_graph("ɹ")
-	document.getElementById("s").value = system.get_graph("s")
-	document.getElementById("sh").value = system.get_graph("ʃ")
-	document.getElementById("t").value = system.get_graph("t")
-	document.getElementById("th").value = system.get_graph("θ")
-	document.getElementById("v").value = system.get_graph("v")
-	document.getElementById("w").value = system.get_graph("w")
-	document.getElementById("y").value = system.get_graph("j")
-	document.getElementById("z").value = system.get_graph("z")
-	document.getElementById("zh").value = system.get_graph("ʒ")
+	for (const index in input_pairs) {
+		const pair = input_pairs[index]
+		pair[1].value = system.get_graph(pair[0])
+	}
 }
 
 function clear() {
 	document.getElementById("systemName").value = ""
 	document.getElementById("systemDescription").value = ""
-	
-	document.getElementById("aa").value = ""
-	document.getElementById("ae").value = ""
-	document.getElementById("uh").value = ""
-	document.getElementById("aw").value = ""
-	document.getElementById("au").value = ""
-	document.getElementById("ai").value = ""
-	document.getElementById("e").value = ""
-	document.getElementById("er").value = ""
-	document.getElementById("schwa").value = ""
-	document.getElementById("schwar").value = ""
-	document.getElementById("ei").value = ""
-	document.getElementById("ih").value = ""
-	document.getElementById("ee").value = ""
-	document.getElementById("oa").value = ""
-	document.getElementById("oi").value = ""
-	document.getElementById("horseshoe").value = ""
-	document.getElementById("oo").value = ""
-	document.getElementById("b").value = ""
-	document.getElementById("ch").value = ""
-	document.getElementById("d").value = ""
-	document.getElementById("dh").value = ""
-	document.getElementById("f").value = ""
-	document.getElementById("g").value = ""
-	document.getElementById("h").value = ""
-	document.getElementById("j").value = ""
-	document.getElementById("k").value = ""
-	document.getElementById("l").value = ""
-	document.getElementById("m").value = ""
-	document.getElementById("n").value = ""
-	document.getElementById("ng").value = ""
-	document.getElementById("p").value = ""
-	document.getElementById("r").value = ""
-	document.getElementById("s").value = ""
-	document.getElementById("sh").value = ""
-	document.getElementById("t").value = ""
-	document.getElementById("th").value = ""
-	document.getElementById("v").value = ""
-	document.getElementById("w").value = ""
-	document.getElementById("y").value = ""
-	document.getElementById("z").value = ""
-	document.getElementById("zh").value = ""
+
+	for (const index in input_pairs) {
+		const pair = input_pairs[index]
+		pair[0].value = ""
+	}
 }
 
 // EXPORTS =========================================
